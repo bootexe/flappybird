c22f37d37312e16406dd1efab21aa5d52c1ebf1c
BootEXE <raphael_attal@icloud.com>
2021-12-15 09:11:34 +0100

    First Commit

---
+++ main.js
@@ -0,0 +1,48 @@
+
+/*
+sleep(1000);
+console.log('hello');
+
+let to = setTimeout(function(){
+    console.log('hello');
+},1000);
+
+clearTimeout(to);
+
+
+
+while(1){
+    console.log('hello');
+    sleep(1000);
+}
+
+let interval = setInterval(function(){
+    console.log('hello');
+},1000);
+clearInterval(interval);
+*/
+
+
+
+let bird = {
+    y:5,
+    status:true
+}
+
+
+let tuyaux= [
+    {
+        y:Math.round(Math.random()*10),
+        x:10,
+        id:0
+    },
+    {
+        y:Math.round(Math.random()*10),
+        x:10,
+        id:1
+    }
+]
+
+function render(){
+    
+}
